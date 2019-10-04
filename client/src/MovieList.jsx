da9ec65957577f5f40a2d508d103853d675cabe3
Angelique Nehmzow <angelique@nehmzow.com>
2019-10-03 23:18:33 -0700

    user can add movies, app displays only user-added movies

--- client/src/MovieList.jsx
+++ client/src/MovieList.jsx
@@ -1,29 +1,38 @@
 import React from 'react';
 import Movie from './Movie';
 
-var initialMovieList = [
-  {title: 'Mean Girls'},
-  {title: 'Hackers'},
-  {title: 'The Grey'},
-  {title: 'Sunshine'},
-  {title: 'Ex Machina'},
-];
+// var initialMovieList = [
+//   {title: 'Mean Girls'},
+//   {title: 'Hackers'},
+//   {title: 'The Grey'},
+//   {title: 'Sunshine'},
+//   {title: 'Ex Machina'},
+// ];
 
 class MovieList extends React.Component {
   constructor(props) {
     super(props);
+    // this.state = {
+    //   movies: initialMovieList,
+    //   searchedMovies: initialMovieList
+    // };
     this.state = {
-      movies: initialMovieList,
-      searchedMovies: initialMovieList
+      movies: [],
+      searchedMovies: []
     };
     this.handleSearch = this.handleSearch.bind(this);
+    this.handleMovieAdd = this.handleMovieAdd.bind(this);
   };
 
   render() {
     return (
       <div>
         <div>
-          <input id="search-bar" type="text"/>
+          <input id="new-movie-input" type="text"/>
+          <button onClick={this.handleMovieAdd}>Add</button>
+        </div>
+        <div>
+          <input id="search-input" type="text"/>
           <button onClick={this.handleSearch}>Search</button>
         </div>
         <div>
@@ -35,14 +44,26 @@ class MovieList extends React.Component {
 
   handleSearch(e) {
     e.preventDefault();
-    var userInput = document.getElementById('search-bar').value;
+    var userInput = document.getElementById('search-input').value;
     var newSearchedMovies = getSearchedMovies(userInput, this.state.movies);
     if (newSearchedMovies.length === 0) {
       alert('No movie by that name found');
-      this.setState({searchedMovies: initialMovieList});
+      this.setState({searchedMovies: this.state.movies}); // re-render view to show all movies
     } else {
       newSearchedMovies = newSearchedMovies.map((movie) => { return {title: movie} } );
-      this.setState({searchedMovies: newSearchedMovies});
+      this.setState({searchedMovies: newSearchedMovies}); // re-render view to just show movies matching search terms
+    }
+  }
+
+  handleMovieAdd(e) {
+    e.preventDefault();
+    var newMovie = document.getElementById('new-movie-input').value.trim();
+    if (newMovie) {
+      var newMovies = this.state.movies.concat([{title: newMovie}]);
+      this.setState({
+        movies: newMovies,
+        searchedMovies: newMovies // re-render view to show all movies including new one
+      });
     }
   }
 }
